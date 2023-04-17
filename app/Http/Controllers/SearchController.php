5e97af8a006081e58998eebe53842beb568a9e6d
dumbocan <dumbo.cathhotmail.com>
2023-04-18 00:37:23 +0100

    mejorar cajon busqueda

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,7 +9,7 @@ class SearchController extends Controller
     public function show(Request $request)
     {
         $search = $request->input('search');
-        var_dump($search);
+
         return view('search')->with('search', $search);
 
     }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,9 +44,23 @@
 
             <!-- search box -->
 
+            <div class="flex items-center justify-center ">
+               {{ Form::open(array('route' => 'search', 'method' => 'POST')) }}
+                    @csrf
+                    <div class="flex border-2 rounded-xl">
+                        <input id="search" name="search" type="text" class="px-3 py-1 w-55 rounded-xl" placeholder="Search...">
+                        <button type="submit" class="flex items-center justify-center px-4 border-l" >
+                            <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"viewBox="0 0 24 24">
+                            <path
+                            d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                            </svg>
+                        </button>
+                    </div>
+                 {{ Form::close() }}
+            </div>
 
 
-            <div class="flex">
+        <!--    <div class="flex">
                 {{ Form::open(array('route' => 'search', 'method' => 'POST')) }}
                     @csrf
                     <x-text-input id="search" name="search" type="text" class=" mt-2 border border-gray-700 w-64" />
@@ -58,7 +72,7 @@
             </div>
 
 
-            <!-- Settings Dropdown -->
+             Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
