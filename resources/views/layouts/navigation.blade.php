44a83f6ad5618a312bb5ceb8e04fbd74a18dc506
dumbocan <dumbo.cathhotmail.com>
2023-04-19 09:00:24 +0100

    readme.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,11 +44,11 @@
 
             <!-- search box -->
 
-            <div class="flex items-center justify-center ">
+            <div class="sm:ml-10 flex items-center justify-center ">
                {{ Form::open(array('route' => 'search', 'method' => 'POST')) }}
                     @csrf
                     <div class="flex border-2 rounded-xl">
-                        <input id="search" name="search" type="text" class="px-3 py-1 w-55 rounded-xl" placeholder="Search...">
+                        <input id="search" name="search" type="text" class="px-3 py-1 sm:w-52 w-36 rounded-xl" placeholder="Search...">
                         <button type="submit" class="flex items-center justify-center px-4 border-l" >
                             <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"viewBox="0 0 24 24">
                             <path
